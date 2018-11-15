f1f12afe428688f4e4eb392d506604e826309445
Ubuntu <ubuntu@ip-172-31-30-217.us-west-2.compute.internal>
2018-11-15 14:26:41 +0000

    9

---
+++ 0009.palindrome-number/palindrome-number.go
@@ -0,0 +1,19 @@
+package problem0009
+
+import "strconv"
+
+func isPalindrome(x int) bool {
+    if x < 0 { return false}
+    if x == 0 { return true }
+
+    s := strconv.Itoa(x)
+    for i:= range(s){
+        if i >= len(s) / 2 {
+            break
+        }
+        if s[i] != s[len(s)-i-1] {
+            return false
+        }
+    }
+    return true
+}
