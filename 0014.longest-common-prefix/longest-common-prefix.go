1fcd7dbdbe47af947d242022639b93ccb5db92ce
Ubuntu <ubuntu@ip-172-31-30-217.us-west-2.compute.internal>
2018-11-17 12:23:29 +0000

    14

---
+++ 0014.longest-common-prefix/longest-common-prefix.go
@@ -0,0 +1,18 @@
+package problem0014
+
+func longestCommonPrefix(strs []string) string {
+    res := ""
+    j := 0
+    if len(strs) == 0 {return ""}
+    for  {
+        for i := 0; i < len(strs); i++{
+            if j >= len(strs[i]) { return res}
+        }
+        for i := 1; i < len(strs); i++{
+            if strs[i][j] != strs[0][j] {return res}
+        }
+        res += string(strs[0][j])
+        j++
+    }
+	return res
+}
