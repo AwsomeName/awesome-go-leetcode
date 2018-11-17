466b37ff53dc31d930f30bb03056e870b774e756
Ubuntu <ubuntu@ip-172-31-30-217.us-west-2.compute.internal>
2018-11-17 09:03:26 +0000

    0011 O(n) solution

--- 0011.container-with-most-water/container-with-most-water.go
+++ 0011.container-with-most-water/container-with-most-water.go
@@ -1,22 +1,33 @@
 package problem0011
 
 func maxArea(height []int) int {
+	// 从两端开始寻找，至少保证了宽度是最大值
+	i, j := 0, len(height)-1
+	max := 0
 
-    max := 1
-    tmp := 0
-    for i := 0; i < len(height)-1; i++{
-        for j := i + 1; j < len(height); j++ {
-            tmp = min(height[i],height[j])
-            tmp = tmp * (j - i)
-            if tmp > max { max = tmp}
-        }
-    }
-    return max
+	for i < j {
+		a, b := height[i], height[j]
+		h := min(a, b)
+
+		area := h * (j - i)
+		if max < area {
+			max = area
+		}
+
+		// 朝着area具有变大的可能性方向变化。
+		if a < b {
+			i++
+		} else {
+			j--
+		}
+	}
+
+	return max
 }
 
-func min(a, b int) int {
-    if a < b {
-        return a
-    }
-    return b
+func min(i, j int) int {
+	if i <= j {
+		return i
+	}
+	return j
 }
