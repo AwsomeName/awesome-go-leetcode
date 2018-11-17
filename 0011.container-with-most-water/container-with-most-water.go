6451401de4f787ae86616bf4fd9feee930bb58ac
Ubuntu <ubuntu@ip-172-31-30-217.us-west-2.compute.internal>
2018-11-17 08:55:57 +0000

    0011

--- 0011.container-with-most-water/container-with-most-water.go
+++ 0011.container-with-most-water/container-with-most-water.go
@@ -1,4 +1,22 @@
 package problem0011
 
 func maxArea(height []int) int {
+
+    max := 1
+    tmp := 0
+    for i := 0; i < len(height)-1; i++{
+        for j := i + 1; j < len(height); j++ {
+            tmp = min(height[i],height[j])
+            tmp = tmp * (j - i)
+            if tmp > max { max = tmp}
+        }
+    }
+    return max
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
 }
