fd9e1c53c387e1ed627bfcb87013b12cadd4bc4d
Ubuntu <ubuntu@ip-172-31-30-217.us-west-2.compute.internal>
2018-11-20 14:23:59 +0000

    0019

---
+++ 0019.remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.go
@@ -0,0 +1,26 @@
+package problem0019
+
+// ListNode 是节点
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+    if head == nil { return nil }
+
+    ptr_first, ptr_second := head, head
+    for ptr_first.Next != nil {
+        n--
+        if n>=0 {
+            ptr_second = head
+        }else {
+            ptr_second = ptr_second.Next
+        }
+        ptr_first = ptr_first.Next
+    }
+    if ptr_second.Next == nil { return nil }
+    ptr_second.Next = ptr_second.Next.Next
+
+    return head
+}
