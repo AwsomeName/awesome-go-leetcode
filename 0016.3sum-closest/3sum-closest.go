29f9ac901dcfe946e710406104645a963b853398
Ubuntu <ubuntu@ip-172-31-30-217.us-west-2.compute.internal>
2018-11-20 03:28:28 +0000

    0016 version=0.6

--- 0016.3sum-closest/3sum-closest.go
+++ 0016.3sum-closest/3sum-closest.go
@@ -1,22 +1,23 @@
 package problem0016
 
 import "sort"
+import "fmt"
 
 func threeSumClosest(nums []int, target int) int {
     sort.Ints(nums)
     result := nums[0] + nums[1] + nums[2]
     for i := range nums{
-        if nums[i] >= target { break }
+        if delta(result,target) == 0 { return result}
         for j := i + 1; j < len(nums)-1; j++{
-            l := j + 1
-            r := len(nums) - 1
-            if l > r { break }
-            for x:=l; x <=r; x++{
+            for x:=j+1; x <=len(nums)-1; x++{
                 if delta(result, target) > delta( nums[i] + nums[j] + nums[x], target) {
                     result = nums[i]+ nums[j] + nums[x]
+                    fmt.Println(result, nums[i], nums[j], nums[x],"-------------")
                 }
+                fmt.Println(result, nums[i], nums[j], nums[x])
             }
             for j < len(nums) && nums[j-1] == nums[j] { j++ }
+
         }
         for i > 0 && i < len(nums) && nums[i-1] == nums[i] { i++ }
     }
