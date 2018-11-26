cac3ae19f9669fedfab6a149dc99519d16626f1d
Ubuntu <ubuntu@ip-172-31-30-217.us-west-2.compute.internal>
2018-11-26 03:08:52 +0000

    0029 recorrect

--- 0029.divide-two-integers/divide-two-integers.go
+++ 0029.divide-two-integers/divide-two-integers.go
@@ -2,18 +2,19 @@ package problem0029
 
 import "math"
 
-func divide(m, n int) int {
-    if n == 0 { return math.MaxInt32 }
-    if m == 0 { return 0 }
+func divide(dividend int, divisor int) int {
+    if divisor  == 0 { return math.MaxInt32 }
+    if dividend == 0 { return 0 }
     var res int
     var mSign,nSign,resSign int
-    if m > 0 {mSign = 1} else if m < 0 { mSign = -1}
-    if n > 0 {nSign = 1} else if n < 0 { nSign = -1}
+    if dividend > 0 {mSign = 1} else if dividend < 0 { mSign = -1}
+    if divisor  > 0 {nSign = 1} else if divisor  < 0 { nSign = -1}
     if mSign + nSign == 0 { resSign = -1 } else { resSign = 1}
-    if m < 0 { m = -m }
-    if n < 0 { n = -n }
-    res,_ = recursion_divide(m,n,1)
-    if res > math.MaxInt32 { return math.MaxInt32}
+    if dividend < 0 { dividend = -dividend }
+    if divisor  < 0 { divisor  = -divisor  }
+    res,_ = recursion_divide(dividend,divisor ,1)
+    if res > math.MaxInt32 && resSign>0 { return math.MaxInt32}
+    if res == math.MaxInt32 && resSign < 0 { return -res }
     if resSign > 0 { return res }else { return -res }
 }
 
