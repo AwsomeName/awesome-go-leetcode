c8e23f72279aea06a4d8ae1b58fe6a835cf0e4d9
Ubuntu <ubuntu@ip-172-31-30-217.us-west-2.compute.internal>
2018-11-28 12:42:52 +0000

    0035

---
+++ 0035.search-insert-position/search-insert-position.go
@@ -0,0 +1,29 @@
+package problem0035
+import "fmt"
+
+func searchInsert(nums []int, target int) int {
+    if len(nums) < 1 {{ return 0}}
+    if len(nums) == 1 {
+        if nums[0] >= target { return 1} else { return 0}
+    }
+
+    left,right := 0, len(nums)-1
+    for left <= right {
+        mid:= (left + right)/2
+        fmt.Println("left:",left,"right:",right,"mid:",mid,"nums:",nums[mid],"target:",target)
+        if nums[mid] == target {
+            return mid
+        }
+        if nums[mid] < target {
+            if mid == len(nums) -1 { return mid+1}
+            if mid < len(nums) -1 && nums[mid+1] > target { return mid+1}
+            left = mid + 1
+        }else {
+            if mid == 0 { return 0 }
+            if mid > 0 && nums[mid-1] < target { return mid }
+            right = mid -1
+        }
+        fmt.Println("-----after-----:","left:",left,"right:",right,"mid:",mid,"nums:",nums[mid],"target:",target)
+    }
+	return left
+}
