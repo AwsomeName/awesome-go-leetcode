45751a331647ecc6c8c936353d643be36011c14b
Ubuntu <ubuntu@ip-172-31-30-217.us-west-2.compute.internal>
2018-12-02 04:24:27 +0000

    0950 adjust

--- 0952.largest-component-size-by-common-factor/deckRevealedIncreasing.go
+++ 0952.largest-component-size-by-common-factor/deckRevealedIncreasing.go
@@ -4,12 +4,18 @@ import "sort"
 //import "fmt"
 
 func deckRevealedIncreasing(deck []int) []int {
-    if len(deck) <= 1 {
+    if len(deck) < 1 {
+        return nil
+    }
+    if len(deck) == 1 {
         return deck
     }
     //res := make([]int, len(deck))
     var res []int
 	sort.Ints(deck)
+    if len(deck)==2 {
+        return deck
+    }
     for i:= len(deck)-1; i >=0; i--{
         if len(res) < 1 {
             res = append(res,deck[i])
