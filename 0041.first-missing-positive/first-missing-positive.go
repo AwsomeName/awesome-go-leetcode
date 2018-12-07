20a4c30dd6447146fbab23d4107e50faea16094c
Ubuntu <ubuntu@ip-172-31-30-217.us-west-2.compute.internal>
2018-12-07 07:34:54 +0000

    0041 again

--- 0041.first-missing-positive/first-missing-positive.go
+++ 0041.first-missing-positive/first-missing-positive.go
@@ -1,14 +1,13 @@
 package problem0041
 
 func firstMissingPositive(nums []int) int {
-	M := [1000]bool{}
-	M[0] = true
+	M := [1100]bool{}
 	for _, x := range nums {
 		if x > 0 {
 			M[x] = true
 		}
 	}
-	for i := 0; i < 1000; i++ {
+	for i := 1; i < 1000; i++ {
 		if M[i] == false {
 			return i
 		}
