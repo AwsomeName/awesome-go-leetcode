ea4b112ec39e41e8456684077b830bb69f242848
Ubuntu <ubuntu@ip-172-31-30-217.us-west-2.compute.internal>
2018-12-10 09:05:19 +0000

    0956

---
+++ 0956.tallestbillboard/tallestBillboard.go
@@ -0,0 +1,58 @@
+package problem0956
+
+import "fmt"
+import "sort"
+
+func sum(rods []int) int {
+	sum := 0
+	for _, x := range rods {
+		sum += x
+	}
+	return sum
+}
+
+func find(rods []int, target int, res *bool) {
+	if *res || target == 0 {
+		*res = true
+		return
+	}
+	if len(rods) <= 0 || target < rods[0] {
+		return
+	}
+	find(rods[1:], target-rods[0], res)
+	find(rods[1:], target, res)
+}
+
+func cnt(rods []int) int {
+	if len(rods) <= 2 && sum(rods)%2 == 1 {
+		return 0
+	}
+	target := sum(rods) / 2
+	res := false
+	find(rods, target, &res)
+	//    if sum(rods)%2 ==0 && find(rods,target){
+	if sum(rods)%2 == 0 && res {
+		fmt.Println("target:", target, "sum:", sum(rods))
+		return target
+	} else {
+		for i := 0; i < len(rods); i++ {
+			nRods := rods[1:]
+			nRods = nRods[:len(nRods):len(nRods)]
+			if i > 0 {
+				nRods[i] = rods[0]
+			}
+			sort.Ints(nRods)
+			r := cnt(nRods)
+			if r > 0 {
+				return r
+			}
+		}
+	}
+	return 0
+}
+
+func tallestBillboard(rods []int) int {
+	fmt.Println("----------", rods, "---------")
+	sort.Ints(rods)
+	return cnt(rods)
+}
---
+++ 0956.tallestbillboard/tallestBillboard_test.go
@@ -0,0 +1,55 @@
+package problem0956
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+type para struct {
+	one []int
+}
+
+type ans struct {
+	ans int
+}
+
+type question struct {
+	p para
+	a ans
+}
+
+func Test_OK(t *testing.T) {
+	ast := assert.New(t)
+
+	qs := []question{
+		question{
+			p: para{
+				one: []int{1, 2, 3, 6},
+			},
+			a: ans{
+				6,
+			},
+		},
+		question{
+			p: para{
+				one: []int{1, 2, 3, 4, 5, 6},
+			},
+			a: ans{
+				10,
+			},
+		},
+		question{
+			p: para{
+				one: []int{1, 2},
+			},
+			a: ans{
+				0,
+			},
+		},
+	}
+
+	for _, q := range qs {
+		a, p := q.a, q.p
+		ast.Equal(a.ans, tallestBillboard(p.one), "input:%v", p)
+	}
+}
