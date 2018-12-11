4bf1c663b0cf8dd938b69d94a86843d1cb9c4efa
AwsomeName <liuchang.hit.cs@gmail.com>
2018-12-11 21:12:04 +0800

    0956 need redo

--- 0956.tallestbillboard/tallestBillboard.go
+++ 0956.tallestbillboard/tallestBillboard.go
@@ -2,63 +2,30 @@ package problem0956
 
 import "fmt"
 import "sort"
-
-func sum(rods []int) int {
-	sum := 0
-	for _, x := range rods {
-		sum += x
-	}
-	return sum
-}
-
-func find(rods []int, target int, res *bool) {
-	if *res || target == 0 {
-		*res = true
-		return
-	}
-	if len(rods) <= 0 || target < rods[0] {
-		return
-	}
-	find(rods[1:], target-rods[0], res)
-	find(rods[1:], target, res)
-}
+import "math"
 
 func cnt(rods []int) int {
-	//fmt.Println("rods:",rods,"-------------------")
-	if len(rods) < 2 || len(rods) == 2 && rods[0] != rods[1] {
-		return 0
-	}
-	target := sum(rods) / 2
-	res := false
-	find(rods, target, &res)
-	if sum(rods)%2 == 0 && res {
-		//		fmt.Println("target:", target, "sum:", sum(rods))
-		return target
-	} else {
-		//fmt.Println("else: target:", target, "sum:", sum(rods),"rods:",rods)
-		res := 0
-		for i := 0; i < len(rods); i++ {
-			nRods := make([]int, len(rods)-1, len(rods)-1)
-			for j := 0; j < len(rods)-1; j++ {
-				if j >= i {
-					nRods[j] = rods[j+1]
-				} else {
-					nRods[j] = rods[j]
-				}
-			}
-			r := cnt(nRods)
-			if len(nRods) == 10 {
-				fmt.Println("nRods:", nRods)
-			}
-			if r > 0 && res < r {
-				res = r
-				if len(nRods) == 10 {
-					fmt.Println("nRods:", nRods, res, rods, "i:", i)
-				}
-			}
-		}
-		return res
-	}
+    var dp func(i,s int)int
+    dp = func(i,s int) int {
+        if i==len(rods) {
+            if s==0 {
+                return 0
+            }else {
+                return -math.MaxInt32
+            }
+        }
+        maxAns := dp(i+1,s)
+        tmp := dp(i+1,s-rods[i])
+        if maxAns < tmp {
+            maxAns = tmp
+        }
+        tmp = dp(i+1,s+rods[i]) + rods[i]
+        if maxAns < tmp {
+            maxAns = tmp
+        }
+        return maxAns
+    }
+    return dp(0,0)
 }
 
 func tallestBillboard(rods []int) int {
