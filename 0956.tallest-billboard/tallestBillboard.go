3d41d514d461864d75cc620a2820b239c9099641
Ubuntu <ubuntu@ip-172-31-30-217.us-west-2.compute.internal>
2018-12-12 07:29:29 +0000

    0956 a

--- 0956.tallest-billboard/tallestBillboard.go
+++ 0956.tallest-billboard/tallestBillboard.go
@@ -2,11 +2,46 @@ package problem0956
 
 import "fmt"
 
+func abs (x int)int{
+    if x>0 {
+        return x
+    } else {
+        return -x
+    }
+}
+
+func max(a,b int) int{
+    if a > b {
+        return a
+    }else {
+        return b
+    }
+}
+
+func min(a,b int) int{
+    if a < b {
+        return a
+    }else {
+        return b
+    }
+}
+
 func tallestBillboard(rods []int) int {
 	fmt.Println("=================", rods, "=====================")
-
-
-
-
-	return cnt(rods)
+    dp := make(map[int]int)
+    dp[0] = 0
+    for _,x := range rods{
+        cur := make(map[int]int)
+        for d,y := range dp{
+            cur[d] = y
+        }
+        //fmt.Println("before dp:",dp)
+        for d,y := range cur{
+            //fmt.Println("d,y,x:",d,y,x)
+            dp[d+x] = max(dp[x+d],y)
+            dp[abs(d-x)] = max(dp[abs(d-x)],y+min(d,x))
+        }
+        //fmt.Println("after dp:",dp)
+    }
+	return dp[0]
 }
