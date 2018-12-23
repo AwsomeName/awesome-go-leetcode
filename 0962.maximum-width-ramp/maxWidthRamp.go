1367a318daef6696c1186e5c70f349411f0a93dd
AwsomeName <liuchang.hit.cs@gmail.com>
2018-12-23 12:46:13 +0800

    0962

--- 0962.maximum-width-ramp/maxWidthRamp.go
+++ 0962.maximum-width-ramp/maxWidthRamp.go
@@ -1,27 +1,41 @@
 package problem0962
 
 func maxWidthRamp(A []int) int {
-    var find func(A []int) int
-    find = func(A []int) int{
-        if len(A) < 2 {
-            return 0
-        }
-        i,j := 0,len(A)-1
-        for i<j {
-            if A[i] <= A[j] {
-                return j-i
-            }else {
-                return max( find(A[1:]), find(A[:len(A)-1]) )
-            }
-        }
-        return 0
+    mark := make(map[int]bool)
+    idx := make([][]int,50001)
+    for i:= range idx{
+        idx[i] = make([]int,2)
     }
-
-    return find(A)
+    for i,x := range A{
+        if mark[x]{
+            idx[x][1] = i
+        }else{
+            mark[x] = true
+            idx[x][0] = i
+            idx[x][1] = i
+        }
+    }
+    ss := 50002
+    ans := 0
+    for i :=0; i<50001; i++{
+        if mark[i]{
+            ss = min(ss,idx[i][0])
+            ans = max(ans,idx[i][1]-ss)
+        }
+    }
+    return ans
 }
 
-func max(a,b int) int{
-    if a > b {
+func max (a,b int) int{
+    if a < b {
+        return b
+    }else {
+        return a
+    }
+}
+
+func min (a,b int) int{
+    if a < b {
         return a
     }else {
         return b
