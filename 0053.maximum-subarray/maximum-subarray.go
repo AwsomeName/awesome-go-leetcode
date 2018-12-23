60027a7f0cc1962d58ba257ee499cc60a959b5fb
AwsomeName <liuchang.hit.cs@gmail.com>
2018-12-23 22:31:52 +0800

    0053

--- 0053.maximum-subarray/maximum-subarray.go
+++ 0053.maximum-subarray/maximum-subarray.go
@@ -1,13 +1,22 @@
 package problem0053
 
 func maxSubArray(nums []int) int {
-	sum, maxSum := -1<<31, -1<<31
-	for _, n := range nums {
-		// sum+n < n，那就还不如直接从 n 开始统计。
-		sum = max(sum+n, n)
-		maxSum = max(maxSum, sum)
+	dp := make([]int, len(nums))
+	dp[0] = nums[0]
+	for i := 1; i < len(nums); i++ {
+		if dp[i-1] > 0 {
+			dp[i] = max(dp[i-1]+nums[i], 0+nums[i])
+		} else {
+			dp[i] = nums[i]
+		}
 	}
-	return maxSum
+	max := math.MinInt32
+	for _, x := range dp {
+		if max < x {
+			max = x
+		}
+	}
+	return max
 }
 
 func max(a, b int) int {
