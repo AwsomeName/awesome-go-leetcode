277a93ecc9d205569e5578c9b9cc6579f36d3594
AwsomeName <liuchang.hit.cs@gmail.com>
2018-12-23 14:42:23 +0800

    0962

--- 0962.maximum-width-ramp/maxWidthRamp.go
+++ 0962.maximum-width-ramp/maxWidthRamp.go
@@ -1,43 +1,27 @@
 package problem0962
 
 func maxWidthRamp(A []int) int {
-    mark := make(map[int]bool)
-    idx := make([][]int,50001)
-    for i:= range idx{
-        idx[i] = make([]int,2)
-    }
-    for i,x := range A{
-        if mark[x]{
-            idx[x][1] = i
-        }else{
-            mark[x] = true
-            idx[x][0] = i
-            idx[x][1] = i
-        }
-    }
-    ss := 50002
-    ans := 0
-    for i :=0; i<50001; i++{
-        if mark[i]{
-            ss = min(ss,idx[i][0])
-            ans = max(ans,idx[i][1]-ss)
-        }
-    }
-    return ans
+	stk := make([]int, 0, 50001)
+	for i, x := range A {
+		if len(stk) == 0 || x < A[stk[len(stk)-1]] {
+			stk = append(stk, i)
+		}
+	}
+	fmt.Println(stk)
+	res := 0
+	for i := len(A) - 1; i >= 0; i-- {
+		for len(stk) > 0 && A[stk[len(stk)-1]] <= A[i] {
+			res = max(res, i-stk[len(stk)-1])
+			stk = stk[:len(stk)-1]
+		}
+	}
+	return res
 }
 
-func max (a,b int) int{
-    if a < b {
-        return b
-    }else {
-        return a
-    }
-}
-
-func min (a,b int) int{
-    if a < b {
-        return a
-    }else {
-        return b
-    }
+func max(a, b int) int {
+	if a < b {
+		return b
+	} else {
+		return a
+	}
 }
